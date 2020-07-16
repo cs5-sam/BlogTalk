5407ce6bbeb4dc2a7b62b9a764b0bfc2778b8279
cs5-sam <54584505+cs5-sam@users.noreply.github.com>
2020-07-16 20:42:20 +0530

    Delete Post

--- app/src/main/java/android/example/blogtalk/Activities/SettingsActivity.java
+++ app/src/main/java/android/example/blogtalk/Activities/SettingsActivity.java
@@ -13,6 +13,7 @@ import android.example.blogtalk.R;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,19 +23,29 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private EditText newPassword;
+    private EditText userNewName;
+    private Button changeBtn;
+    private CircleImageView userNewImage;
     private ProgressBar loadingBar;
-    private Button changePasswordBtn, deactivateAccountBtn;
+
+    static int PReqCode = 3;
+    static int REQUESTCODE = 3;
+    Uri pickedImageUri;
 
     FirebaseAuth auth;
     FirebaseUser user;
@@ -44,85 +55,134 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        newPassword = findViewById(R.id.settings_password_edit_text);
-        changePasswordBtn = findViewById(R.id.change_password_btn);
-        deactivateAccountBtn = findViewById(R.id.deactivate_btn);
+        userNewImage = findViewById(R.id.settings_user_image);
+        userNewName = findViewById(R.id.settings_name_edit_text);
+        changeBtn = findViewById(R.id.settings_change_btn);
         loadingBar = findViewById(R.id.settings_progressBar);
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
-        changePasswordBtn.setVisibility(View.VISIBLE);
         loadingBar.setVisibility(View.INVISIBLE);
+        changeBtn.setVisibility(View.VISIBLE);
 
-        changePasswordBtn.setOnClickListener(new View.OnClickListener() {
+        userNewName.setText(user.getDisplayName());
+        Glide.with(this).load(user.getPhotoUrl()).into(userNewImage);
+
+        userNewImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                changePasswordBtn.setVisibility(View.INVISIBLE);
-                loadingBar.setVisibility(View.VISIBLE);
-                change();
+
+                if(Build.VERSION.SDK_INT >= 22){
+                    checkAndRequestForPermission();
+                }
+                else{
+                    openGallery();
+                }
+
             }
         });
 
-        deactivateAccountBtn.setOnClickListener(new View.OnClickListener() {
+        changeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                deactivateAccountBtn.setVisibility(View.INVISIBLE);
                 loadingBar.setVisibility(View.VISIBLE);
-                deactivate();
+                changeBtn.setVisibility(View.INVISIBLE);
+
+                String name = userNewName.getText().toString();
+
+                if (pickedImageUri == null) {
+                    showMessage("Please choose an image");
+                }
+                else if(name.equals("")){
+                    name = user.getDisplayName();
+                    uploadData(name);
+                }
+                else {
+                    uploadData(name);
+                }
+            }
+        });
+    }
+
+    private void uploadData(final String name){
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("users_photos");
+        final StorageReference imageFilePath = storageReference.child(pickedImageUri.getLastPathSegment());
+        imageFilePath.putFile(pickedImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // image uploaded successfully
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // uri contain user image url
+                        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .setPhotoUri(uri)
+                                .build();
+                        user.updateProfile(profileUpdate)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()){
+                                            // user info updated
+                                            showMessage("Data updated");
+                                            updateUri();
+                                        }
+                                    }
+                                });
+                    }
+                });
             }
         });
 
     }
 
-    public void change(){
-        if(user != null) {
-            user.updatePassword(newPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if(task.isSuccessful()){
-                        showMessage("Password changed successfully!");
-                        changePasswordBtn.setVisibility(View.VISIBLE);
-                        loadingBar.setVisibility(View.INVISIBLE);
-                        auth.signOut();
-                        Intent i = new Intent(SettingsActivity.this,LoginActivity.class);
-                        startActivity(i);
-                        finish();
-                    }
-                    else{
-                        showMessage("Error occurred");
-                        changePasswordBtn.setVisibility(View.VISIBLE);
-                        loadingBar.setVisibility(View.INVISIBLE);
-                    }
-                }
-            });
-        }
-    }
-
-    public void deactivate(){
-        if(user != null){
-            user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                 if(task.isSuccessful()) {
-                        showMessage("Account deleted!");
-                         deactivateAccountBtn.setVisibility(View.VISIBLE);
-                         loadingBar.setVisibility(View.INVISIBLE);
-                         Intent i = new Intent(SettingsActivity.this,LoginActivity.class);
-                         startActivity(i);
-                         finish();
-                    }
-                    else{
-                        showMessage("Could not be deleted");
-                        deactivateAccountBtn.setVisibility(View.VISIBLE);
-                        loadingBar.setVisibility(View.INVISIBLE);
-                    }
-                }
-            });
-        }
+    private void updateUri() {
+        Intent homeIntent = new Intent(SettingsActivity.this,Home.class);
+        startActivity(homeIntent);
+        finish();
     }
 
     private void showMessage(String s) {
         Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    private void openGallery() {
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESTCODE);
+
+    }
+
+    private void checkAndRequestForPermission() {
+        if(ContextCompat.checkSelfPermission(SettingsActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(SettingsActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(this, "Please accept for required permission", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                ActivityCompat.requestPermissions(SettingsActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PReqCode);
+            }
+        }
+        else{
+            openGallery();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == REQUESTCODE && data != null){
+            // the user has successfully picked an image
+            // we save its reference
+            pickedImageUri = data.getData();
+            userNewImage.setImageURI(pickedImageUri);
+
+        }
     }
 }
