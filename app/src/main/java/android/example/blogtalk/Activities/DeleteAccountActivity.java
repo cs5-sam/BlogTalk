8643b7f26380d2bf22369c282fb4235dfc874e94
cs5-sam <54584505+cs5-sam@users.noreply.github.com>
2020-07-17 17:42:51 +0530

    Delete Post

--- app/src/main/java/android/example/blogtalk/Activities/DeleteAccountActivity.java
+++ app/src/main/java/android/example/blogtalk/Activities/DeleteAccountActivity.java
@@ -13,6 +13,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
